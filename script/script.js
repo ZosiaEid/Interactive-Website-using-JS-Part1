96f2f3f580c16bf789c2d2d569b9751d7369fe53
ZosiaEid <zosia.kozlowicz@gmail.com>
2017-06-23 12:13:36 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,16 @@
-document.getElementById("A").onmouseover = cardOnMouseOverHandler;
-document.getElementById("A").onmouseout = cardOnMouseOutHandler;
+document.getElementById("A").onmouseover = cardonmouseOverHandler;
+document.getElementById("A").onmouseout = cardonmouseOutHandler;
 
-function cardOnMouseOverHandler() {
-    document.getElementById("A")images.src = "aligator.jpg";
+function cardonmouseOverHandler() {
+    document.getElementById("A").images.src = "aligator.jpg";
 }
 
-function cardOnMouseOutHandler() {
-    document.getElementById("A")images.src = "A.jpg";
+function cardonmouseOutHandler() {
+    document.getElementById("A").images.src = "A.jpg";
+}
+
+document.getElementById("1").onclick = onclick1Button;
+
+function onclick1Button () {
+	document.getElementById("1").images.src = "A.jpg";
 }
